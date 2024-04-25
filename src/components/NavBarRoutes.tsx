11610b6785ddbe66beeb9e70842c83cca81b5d1b
jr2tan5 <jr2tan_5@hotmail.com>
2024-04-26 03:10:13 +0800

    Updated Pages based on the example in the following link: https://codesandbox.io/p/sandbox/basic-chakra-ui-setup-with-react-router-vhg4w?file=%2Fsrc%2Froutes%2FAboutPage%2Findex.ts

--- src/components/NavBarRoutes.tsx
+++ src/components/NavBarRoutes.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavigationBar from "./NavigationBar"
 
 export default function NavBarRoutes() {
+  const Layout = NavigationBar;
+  const 
     return (
       <BrowserRouter>
         <Routes>
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export * from "./Contact";
+export * from "./Gallery";
+export * from "./Menu";
+export * from './Reservations';
