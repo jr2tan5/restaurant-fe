eeb5ec45adf73d4767ce9ef62bff635a8393597b
jr2tan5 <jr2tan_5@hotmail.com>
2024-04-30 04:26:55 +0800

    - Segregated Menu file into various functions for enhanced readability.

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box, Text, Image, Center, VStack, HStack } from "@chakra-ui/react";
+import React, { JSXElementConstructor } from "react";
+import { Box, Text, Image, Center, VStack, HStack, Table, Heading, Tbody, Tr, Td } from "@chakra-ui/react";
 import NavigationBar from "../components/NavigationBar";
 import beef_noodle from "../assets/beef_noodle.jpg"
 
@@ -7,6 +7,17 @@ interface Dish {
     description: string
     chineseDescription: string
     price: string
+}
+
+const MenuTableData = ({ description, chineseDescription, price }: Dish) => {
+    return (
+        <Td>
+            <HStack>
+                <Text>{description}</Text>
+                <Text>{price}</Text>
+            </HStack>
+        </Td>
+    )
 }
 
 const WhiteDivider = () => {
@@ -17,85 +28,91 @@ const WhiteDivider = () => {
     )
 }
 
-const MenuItem = ({ description, chineseDescription, price }: Dish) => {
+const MenuPageHeader = () => {
     return (
-        <HStack>
-            <Text>{description}</Text>
-            <Text>{price}</Text>
-        </HStack>
+        <Box bgImage={beef_noodle}
+            bgPosition={"middle left"}
+            bgSize={"cover"}
+            width={"100%"}
+            height={"225px"}>
+            <NavigationBar />{ }
+        </Box>
     )
+}
+
+const AppetizerBanner = () => {
+    return (
+        <Center>
+            <Box
+                display={"flex"}
+                position={"relative"}
+                width={"80%"}
+                height={"500px"}
+                alignItems={"justify"}>
+                <Image src={beef_noodle} alt="Signature Beef Noodle" objectFit={"cover"} width={"100%"} height={"100%"} />
+                <Box
+                    position="absolute"
+                    bottom="50px"  // Position from bottom
+                    left="50px"    // Position from left
+                    color="white"
+                    fontSize="xl"
+                    fontWeight="bold">
+                    <Heading>Our Menu</Heading>
+                    <Text>J Restaurant is all about showcasing the best of Southern Chinese Cuisine</Text>
+                </Box>
+            </Box>
+        </Center>
+    )
+}
+
+const AppetizerMenu = () => {
+    return (
+        <Center>
+            <Box
+                width={"80%"}
+                position={"relative"}
+            >
+                <VStack>
+                    <Heading
+                        textAlign={"start"}>
+                        Appetizer Specialty
+                    </Heading>
+                    <Table>
+                        <Tbody>
+                            <Tr>
+                                <MenuTableData
+                                    description="Signature Beef Noodle"
+                                    chineseDescription=""
+                                    price="$10"
+                                /><MenuTableData
+                                    description="Signature Pork Noodle"
+                                    chineseDescription=""
+                                    price="$10"
+                                />
+                                <MenuTableData
+                                    description="Signature Chicken Noodle"
+                                    chineseDescription=""
+                                    price="$10"
+                                />
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </VStack>
+            </Box>
+        </Center>
+    )
+
 }
 
 const Menu = () => {
     return (
         <>
-            <Box bgImage={beef_noodle}
-                bgPosition={"middle left"}
-                bgSize={"cover"}
-                width={"100%"}
-                height={"225px"}>
-                <NavigationBar />{ }
-                <Text>Menu</Text>
-            </Box>
+            <MenuPageHeader />
             <WhiteDivider />
-            <Center>
-                <Box
-                    display={"flex"}
-                    justifyContent={"center"}
-                    alignContent={"center"}
-                    position={"relative"}
-                    width={"80%"}
-                    height={"500px"}
-                    alignItems={"justify"}>
-                    <Image src={beef_noodle} alt="Signature Beef Noodle" objectFit={"cover"} width={"100%"} height={"100%"} />
-                    <Text
-                        position="absolute"
-                        bottom="50px"  // Position from bottom
-                        left="50px"    // Position from left
-                        color="white"
-                        fontSize="xl"
-                        fontWeight="bold"
-                    >
-                        Our Menu
-                        <br />
-                        J Restaurant is all about showcasing the best of Southern Chinese Cuisine
-                    </Text>
-                </Box>
-            </Center>
+            <AppetizerBanner />
             <WhiteDivider />
-            <Box
-                display={"flex"}
-                justifyContent={"center"}
-                alignContent={"center"}
-                position={"relative"}
-                width={"80%"}
-                height={"500px"}
-                alignItems={"justify"}>
-                <VStack
-                    alignContent={"left"}>
-                    <HStack>
-                        <MenuItem
-                            description="Signature Beef Noodle"
-                            chineseDescription=""
-                            price="$10"
-                        />
-                        <MenuItem
-                            description="Signature Pork Noodle"
-                            chineseDescription=""
-                            price="$10"
-                        />
-                        <MenuItem
-                            description="Spicy Beef Noodle"
-                            chineseDescription=""
-                            price="$10"
-                        />
-
-                    </HStack>
-                    <HStack></HStack>
-                    <HStack></HStack>
-                    <HStack></HStack>
-                </VStack>
-            </Box>
+            <AppetizerMenu />
+            <WhiteDivider />
         </>// To be completed
     );
 }
